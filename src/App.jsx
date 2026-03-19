8f96f30696186978953f96c064ce7f7fbaa5f945
Carlos Mantilla <carlosmcot@gmail.com>
2026-03-18 20:36:19 -0500

    Feat: Add base project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-red-600 bg-purple-600 p-10 text-center">
+        ¡Tailwind está funcionando! 🎉
+      </h1>
+    </>
+  );
+}
+
+export default App;
