37b0e3e8d4a0313cb0cb0ef6e8c749301ad747b1
David Aceros <142636565+AcerosDavid@users.noreply.github.com>
2026-03-27 23:19:16 -0500

    Feat: Add services for module of Assembly

---
+++ src/modules/ModuleAssembly/services/index.js
@@ -0,0 +1,11 @@
+// Exportar constantes específicas del módulo Armando
+export { ASSEMBLY_LEVELS, MODULE_INFO, PROGRESS_MESSAGES } from './assemblyConstants';
+
+// Exportar utilidades específicas del módulo Armando
+export {
+  getLevelStatus,
+  getLevelSubtitle,
+  getProgressPercentage,
+  getProgressMessage,
+  getLevelConfig
+} from './assemblyUtils';
